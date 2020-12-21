7010e5c60ab4c9153608f768540865a1be6d8396
Tyler Langties <tylerlangties@gmail.com>
2020-12-21 17:03:41 -0500

    remove unused route

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import MyPlaylists from '../views/MyPlaylists.vue'
-import ProjectTwo from '../views/ProjectTwo.vue'
 import PathNotFound from '../views/PathNotFound.vue'
 
 const routes = [
