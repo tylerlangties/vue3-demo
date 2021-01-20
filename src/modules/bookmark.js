b98362d4ad9804c3080330c6baab7e396dc5d58b
Tyler Langties <tylerlangties@gmail.com>
2021-01-20 16:25:52 -0500

    add bookmarks module

---
+++ src/modules/bookmark.js
@@ -0,0 +1,27 @@
+import { ref, readonly } from 'vue'
+
+let bookmarks = ref([])
+
+export default function useBookmarks() {
+  const addBookmark = item => {
+    if (item && !isBookmarked(item)) bookmarks.value.push(item)
+  }
+
+  const removeBookmark = item => {
+    if (isBookmarked(item))
+      bookmarks.value = bookmarks.value.filter(
+        bookmark => bookmark.id !== item.id
+      )
+  }
+
+  const isBookmarked = item => {
+    return bookmarks.value.find(bookmark => bookmark.id === item.id)
+  }
+
+  return {
+    ...readonly(bookmarks),
+    addBookmark,
+    removeBookmark,
+    isBookmarked
+  }
+}
