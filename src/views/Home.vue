6c6017132bb1216b6ec9f419df066148f26578a6
Tyler Langties <tylerlangties@gmail.com>
2021-01-20 16:31:17 -0500

    add greeting to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div v-if="loading" class="loading-box"><Spinner /></div>
   <main class="Home">
     <div v-if="!loading">
+      <h1 class="text-3xl pb-10">Good afternoon, {{ user }}</h1>
       <h1 class="text-3xl font-bold">What to learn next</h1>
       <section id="recommended" class="Home__Recommended">
         <h3 class="text-2xl pt-2">Recommended for you</h3>
@@ -19,6 +20,7 @@
 import { ref } from 'vue'
 import { posts } from '/@/api'
 import usePromiseFn from '/@/composables/use-promise'
+import useAuth from '../modules/auth'
 
 import Carousel from '/@/components/Carousel.vue'
 import Spinner from '/@/components/Spinner.vue'
@@ -30,6 +32,7 @@ export default {
     Spinner
   },
   setup() {
+    const { user } = useAuth()
     //example of API calls using dedicated service files and composables (no VUEX)
     const allPosts = ref([])
     const { loading, error, use: getAllPosts } = usePromiseFn(() => {
@@ -46,7 +49,8 @@ export default {
     return {
       allPosts,
       error,
-      loading
+      loading,
+      user
     }
   }
 }
