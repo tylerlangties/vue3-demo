39f348df4d3c1ffedf49ef8d334dae577b49521e
Tyler Langties <tylerlangties@gmail.com>
2021-01-21 16:27:58 -0500

    add tag and taglist components

---
+++ src/components/UI/Tag.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="text-sm inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-blue-200 text-blue-700 rounded-full"
+  >
+    {{ label }}
+    <i
+      v-if="dismissable"
+      @click="dismiss"
+      class="fas fa-times ml-1 cursor-pointer"
+    ></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  emits: ['dismissed'],
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    dismissable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, { emit }) {
+    const dismiss = () => emit('dismissed', props.label)
+    return { dismiss }
+  }
+}
+</script>
---
+++ src/components/UI/Taglist.vue
@@ -0,0 +1,36 @@
+<template>
+  <Tag
+    v-for="item in modelValue"
+    dismissable
+    @dismissed="removeTag"
+    :key="item"
+    :label="item"
+    class="m-0.5"
+  />
+</template>
+
+<script>
+import { computed, onMounted } from 'vue'
+import Tag from './Tag.vue'
+export default {
+  name: 'Taglist',
+  emits: ['update:modelValue'],
+  components: {
+    Tag
+  },
+  props: {
+    modelValue: {
+      type: Array,
+      required: true,
+      description: 'An array of strings to display'
+    }
+  },
+  setup(props, { emit }) {
+    const removeTag = value => {
+      const filteredTags = props.modelValue.filter(tag => tag !== value)
+      emit('update:modelValue', filteredTags)
+    }
+    return { removeTag }
+  }
+}
+</script>
